38fe2891d4b225fb3f4cc924a5316025eaaa5766
Bohdan <bohdan96@gmail.com>
2019-08-28 21:11:49 +0300

    A lot of noise survives. Turned on trackers efficiencies and reset calorimeter efficiency calculated by sigmoid. But 2 sigma cut-off is left. Checking different physics processes

--- src/LCRunAction.cc
+++ src/LCRunAction.cc
@@ -87,11 +87,11 @@ void LCRunAction::FillEventData(const G4Event* event, LCHitsCollection *HitsColl
     LCHit *hit;
 
     //Simulation of efficiency of calorimeter
-    // G4double S0_cal = 1.72418;
-    // G4double p1_cal = 0.804531;
+    G4double S0_cal = 1.72418;
+    G4double p1_cal = 0.804531;
     // Paper numbers
-    G4double S0_cal = 0.819;
-    G4double p1_cal = 2.166;
+    // G4double S0_cal = 0.819;
+    // G4double p1_cal = 2.166;
 
     G4double S0_tr1 = 0.659858;
     G4double p1_tr1 = 0.166297;
@@ -120,8 +120,8 @@ void LCRunAction::FillEventData(const G4Event* event, LCHitsCollection *HitsColl
         if(layer > 1){
             if(G4UniformRand() > (1. + std::erf((energy_in_mips - S0_cal) / p1_cal)) * p0) continue;
         }
-        // if(hit->GetLayer() == 1 && G4UniformRand() > (1. + std::erf((energy_in_mips - S0_tr2) / p1_tr2)) * p0) continue;
-        // if(hit->GetLayer() == 0 && G4UniformRand() > (1. + std::erf((energy_in_mips - S0_tr1) / p1_tr1)) * p0) continue;
+        if(hit->GetLayer() == 1 && G4UniformRand() > (1. + std::erf((energy_in_mips - S0_tr2) / p1_tr2)) * p0) continue;
+        if(hit->GetLayer() == 0 && G4UniformRand() > (1. + std::erf((energy_in_mips - S0_tr1) / p1_tr1)) * p0) continue;
     
         hit_sector.push_back(sector);
         hit_pad.push_back(pad);
